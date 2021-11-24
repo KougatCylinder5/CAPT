7aec2ee3ae0befac86f60a992c417f4cfab00235
KougatCylinder5 <58046422+KougatCylinder5@users.noreply.github.com>
2021-11-24 10:22:48 -0500

    Update MoCap V0.0.5.py

--- MoCap V0.0.5.py
+++ MoCap V0.0.5.py
@@ -36,8 +36,10 @@ state = numpy.array([0,0])
 cv2.setMouseCallback("LiveFeed",callback,state)
 
 vid = cv2.VideoCapture(0)
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+vid.set(cv2.CAP_PROP_AUTO_EXPOSURE,0.25)
+vid.set(cv2.CAP_PROP_EXPOSURE, -5.0)
+#vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+#vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
 def recalibrate(value):
@@ -51,14 +53,14 @@ def recalibrate(value):
 
 cv2.createTrackbar("Recalibrate","LiveFeed",0,1,recalibrate)
 
-maxred1 = (10,255,255)
-minred1 = (0,90,0)
+maxred1 = (11,255,255)
+minred1 = (4,160,0)
 maxred2 = (255,255,255)
 minred2 = (255,255,255)
-maxgreen = (100,255,255)
-mingreen = (50,75,0)
-maxblue = (130,255,255)
-minblue = (70,150,0)
+maxgreen = (90,255,255)
+mingreen = (57,75,20)
+maxblue = (120,255,255)
+minblue = (100,70,0)
 
 recalibrate(1)
 
@@ -67,11 +69,9 @@ while(cv2.waitKey(1) != 27):
     if(not ret):
         print("Broke")
         break
+    ogFrame = frame.copy()
     frame = cv2.blur(frame, (15,15),cv2.BORDER_DEFAULT)
-    
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    print(hsv[0,0])
-    
     cv2.imshow("blur",frame)
     redParts1 = cv2.inRange(hsv,minred1,maxred1)
     redParts2 = cv2.inRange(hsv,minred2,maxred2)
@@ -86,21 +86,32 @@ while(cv2.waitKey(1) != 27):
     if(M["m00"] != 0):
         cXR = int(M["m10"] / M["m00"])
         cYR = int(M["m01"] / M["m00"])
-        cv2.circle(frame,(cXR, cYR),15,(0,0,255),-1)
+        cv2.circle(ogFrame,(cXR, cYR),15,(0,0,255),-1)
+        if(cYR < 480 and cXR < 600):
+            #print(cXR,cYR,hsv[cYR,cXR])
+            middle = hsv[cYR,cXR][1] + 30
+            if(middle > 255):
+                middle = 255
+            #print(tuple([hsv[cYR,cXR][0] - 2,200,0]),tuple([hsv[cYR,cXR][0] + 2,255,255]))
     M = cv2.moments(greenParts)
     cXG = None
     if(M["m00"] != 0):
         cXG = int(M["m10"] / M["m00"])
         cYG = int(M["m01"] / M["m00"])
-        cv2.circle(frame,(cXG, cYG),15,(0,255,0),-1)
-    
+        cv2.circle(ogFrame,(cXG, cYG),15,(0,255,0),-1)
     if(cXG != None and cXR != None):    
-        cv2.line(frame,(cXG,cYG),(cXR,cYR),(255,255,255),10)
+        cv2.line(ogFrame,(cXG,cYG),(cXR,cYR),(255,255,255),10)
     M = cv2.moments(blueParts)
+    cXB = None
     if(M["m00"] != 0):
         cXB = int(M["m10"] / M["m00"])
         cYB = int(M["m01"] / M["m00"])
-        cv2.circle(frame,(cXB, cYB),5,(255,0,0),-1)
-    cv2.imshow("LiveFeed",frame)
+        cv2.circle(ogFrame,(cXB, cYB),15,(255,0,0),-1)
+        print(cXB,cYB,hsv[cYB,cXB])
+    
+    if(cXG != None and cXB != None):    
+        cv2.line(ogFrame,(cXG,cYG),(cXB,cYB),(255,255,255),10)
+        
+    cv2.imshow("LiveFeed",ogFrame)
 cv2.destroyAllWindows()
 vid.release()
